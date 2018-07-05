89967e89d544e307707bacc583617b3dd86f5aa2
jorgefloresq <jorge.noel.flores@gmail.com>
2018-07-04 23:01:26 -0700

    Import external components into main app file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import logo from './qtip.png';
-import './App.css';
+import { About } from './components/about';
+import { ArtImage } from './components/art-image';
+import { ConnectAuth } from './components/connect-auth';
+import { Lyrics } from './components/lyrics';
+import { Options } from './components/options';
+import { SideMenu } from './components/side-menu';
+import { SongInfo } from './components/song-info';
+import { ViewArtInfo } from './components/view-art-info';
+import { ViewArt } from './components/view-art';
+import { ViewLyrics } from './components/view-lyrics';
 
 class App extends Component {
   render() {
