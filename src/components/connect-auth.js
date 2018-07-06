eabb5604d40b009514765f7c171a884133ac9962
jorgefloresq <jorge.noel.flores@gmail.com>
2018-07-06 15:49:06 -0700

    Populated connect-auth component

--- src/components/connect-auth.js
+++ src/components/connect-auth.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 
 export class ConnectAuth extends Component {
   render() {
     return (
       <div>
-        <p>Connect Auth</p>
+        <h2>Connect with Spotify</h2>
+        <Button bsStyle="success" bsSize="large" >Connect</Button>
       </div>
     );
   }
