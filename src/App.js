3655e65630cd784e812784598bf929d3d0e405eb
jorgefloresq <jorge.noel.flores@gmail.com>
2018-07-07 20:18:57 -0700

    Cleaned up App.js

--- src/App.js
+++ src/App.js
@@ -16,16 +16,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello World!</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://www.youtube.com/watch?v=LnJQseodrXM&feature=youtu.be">
-          <img src="https://emojipedia-us.s3.amazonaws.com/thumbs/120/twitter/141/smiling-face-with-sunglasses_1f60e.png"/>  
-        </a>
+        <ConnectAuth />
       </div>
     );
   }
