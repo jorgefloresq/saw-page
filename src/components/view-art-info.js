6395c9bb407a502791115359e8bf60308e23d794
Gio <giovanni.sanchez@cetys.edu.mx>
2018-07-10 15:38:05 -0700

    added view art info component

--- src/components/view-art-info.js
+++ src/components/view-art-info.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import { SongInfo } from './song-info';
+import '../css/view-art-info.css';
 
 export class ViewArtInfo extends Component {
   render() {
     return (
-      <div>
-        <p>View Art Info</p>
+      <div className="vai-container">
+        <div className="vai-content">
+          <div className="vai-art">
+            <img src="https://f4.bcbits.com/img/a2351616332_16.jpg"/>
+          </div>
+          <div className="vai-info">
+            <SongInfo/>
+          </div>
+        </div>
       </div>
     );
   }
