f73f6fc26f330445c95225c97781c11d38e2c92e
Gio <giovanni.sanchez@cetys.edu.mx>
2018-07-12 14:20:49 -0700

    added default and blurred backgrounds

--- src/components/view-art-info.js
+++ src/components/view-art-info.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
 import { SongInfo } from './song-info';
 import '../css/view-art-info.css';
+import { defaultB, dynamic, blurred } from './background-styles';
 
 export class ViewArtInfo extends Component {
   render() {
+    let background = {};
+    let color;
+    if (this.props.background === 'default-background') {
+      background = {...defaultB};
+      color = 'white';
+    }
+    else if (this.props.background === 'dynamic-background') {
+      background = {...dynamic};
+    }
+    else {
+      background = { 
+        background: `url(${this.props.albumArt})`,
+        ...blurred
+      };      
+    }
     return (
-      <div className="vai-container">
-        <div className="vai-content">
-          <div className="vai-art">
-            <img src="https://f4.bcbits.com/img/a2351616332_16.jpg"/>
-          </div>
-          <div className="vai-info">
-            <SongInfo/>
+      <div>
+        <div className="vai-container">
+          <div className="vai-content">
+            <div className="vai-art">
+              <img src={this.props.albumArt}/>
+            </div>
+            <div style={{color: color}} className="vai-info">
+              <SongInfo/>
+            </div>
           </div>
         </div>
+        <div style={background}></div>
       </div>
     );
   }
--- src/components/view-art.js
+++ src/components/view-art.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 import '../../src/css/view-art.css';
+import { defaultB, dynamic, blurred } from './background-styles';
 
 export class ViewArt extends Component {
   render() {
+    let background = {};
+    if (this.props.background === 'default-background') {
+      background = {...defaultB};
+    }
+    else if (this.props.background === 'dynamic-background') {
+      background = {...dynamic};
+    }
+    else {
+      background = { 
+        background: `url(${this.props.albumArt})`,
+        ...blurred
+      };      
+    }
     return (
-      <div className="square">
-        <div className="content">
-         <img src="https://f4.bcbits.com/img/a2351616332_16.jpg"/>
+      <div>
+        <div className="square">
+          <div className="content">
+          <img src={this.props.albumArt}/>
+          </div>
         </div>
+        <div style={background}></div>
       </div>
     );
   }
