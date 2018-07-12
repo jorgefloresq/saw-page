9a3937433dcbde5117b5acdc382aae605f18a7e7
Gio <giovanni.sanchez@cetys.edu.mx>
2018-07-12 14:17:02 -0700

    added menu buttons active state

--- src/components/main-menu.js
+++ src/components/main-menu.js
@@ -7,7 +7,6 @@ import { ViewArtInfo } from './view-art-info';
 import { ViewLyrics } from './view-lyrics';
 
 let navbar = {
-  textAlign: 'left',
   background: 'transparent',
   borderStyle: 'none'
 };
@@ -17,7 +16,7 @@ let separator = {
   height: 1.5,
   background: 'white',
   marginTop: 30
-}
+};
 let style = {
   navbar: navbar,
   separator: separator
@@ -28,9 +27,9 @@ let viewOptions = [
   { glyph: 'glyphicon glyphicon-th-large', text: 'ViewLyrics' },
 ];
 let backgroundOptions = [
-  { glyph: 'glyphicon glyphicon-adjust', text: 'Black background' },
-  { glyph: 'glyphicon glyphicon-calendar', text: 'Dynamic background' },
-  { glyph: 'glyphicon glyphicon-tasks', text: 'Blurred background' }
+  { glyph: 'glyphicon glyphicon-adjust', text: 'Default background', class: 'default-background' },
+  { glyph: 'glyphicon glyphicon-calendar', text: 'Dynamic background', class: 'dynamic-background' },
+  { glyph: 'glyphicon glyphicon-tasks', text: 'Blurred background', class: 'blurred-background' }
 ];
 
 export class MainMenu extends Component {
@@ -38,38 +37,117 @@ export class MainMenu extends Component {
     super(props);
     // function binding
     this.toggleModal = this.toggleModal.bind(this);
-    this.changeView = this.changeView.bind(this);
+    this.handleMenuButton = this.handleMenuButton.bind(this);
+    this.handleMenuItem = this.handleMenuItem.bind(this);
     this.changeBackground = this.changeBackground.bind(this);
-    this.menuIsOpen = this.menuIsOpen.bind(this);
+    this.handleMenuStateChange = this.handleMenuStateChange.bind(this);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.hideButtons = this.hideButtons.bind(this);
     // state object
     this.state = {
       showAbout: true,
       showModal: false,
-      view: 'ViewArt'
+      showMenu: true,
+      menuIsOpen: false,
+      view: 'ViewArt',
+      background: 'default-background',
+      albumArt: 'https://i.scdn.co/image/26321d3457df376afade43b63104119b6ca6db60'
     };
   }
-  menuIsOpen(state) {
-    this.setState({ showAbout: !state.isOpen });
+  // wait counter (wait 3s to hide menu buttons)
+  timeout;
+
+  componentDidMount() {
+      // mouse move event listener
+    document.addEventListener("mousemove", this.handleMouseMove);
+    // hide menu buttons
+    this.hideButtons();
   }
+
+  handleMouseMove() {
+    // reset timeout
+    clearTimeout(this.timeout);
+    // show menu buttons
+    this.setState({
+      showAbout: true,
+      showMenu: true
+    }, ()=> {
+      // set timeout to hide menu buttons
+      this.timeout = this.hideButtons();
+    });    
+  }
+
+  hideButtons() {
+    return setTimeout(()=> {
+      // hide menu buttons
+      this.setState({ 
+        showAbout: false,
+        showMenu: false
+      });      
+    }, 3000);
+  }
+
+  handleMenuButton() {
+    // open menu
+    this.setState({ menuIsOpen: true });
+  }
+
+  handleMenuStateChange(state) {
+    clearTimeout(this.timeout);
+    this.setState({ 
+      // toggle about button
+      showAbout: !state.isOpen,
+      // toggle menu button
+      showMenu: !state.isOpen,
+      // change menu isOpen prop
+      menuIsOpen: state.isOpen
+     }, ()=> {
+       // set timeout to hide menu buttons
+      this.timeout = this.hideButtons();
+     });
+  }
+
+  handleMenuItem(view) {
+    this.setState({
+      // change view
+      view: view,
+      // close menu
+      menuIsOpen: false,
+     });    
+  }
+
+  changeBackground(background) {
+    // change background
+    this.setState({ 
+      // change background
+      background: background,
+      // close menu
+      menuIsOpen: false,
+    });
+    
+  }
+
   toggleModal(toggleValue) {
+    // toggle about modal
     this.setState({ showModal: toggleValue });
   }
-  changeView(view) {
-    this.setState({ view: view });
-  }
-  changeBackground() {
-    // change background
-  }
-  render() {
+
+  render() { 
     return (
       <div>
+        {/* menu button */}
+        { this.state.showMenu && 
+          <Button bsSize="large" onClick={this.handleMenuButton} className="nav-menu">
+            <Glyphicon glyph="glyphicon glyphicon-menu-hamburger"/>
+          </Button>
+        }
         {/* menu */}
-        <Menu right onStateChange={this.menuIsOpen}>
+        <Menu right isOpen={this.state.menuIsOpen} onStateChange={this.handleMenuStateChange}>
           {/* view options */}
           <ButtonGroup vertical block bsSize="large" className="side-menu-button-group">
             {
               viewOptions.map( option =>
-                <Button onClick={()=> this.changeView(option.text)}>
+                <Button onClick={()=> this.handleMenuItem(option.text)}>
                   <Row>
                     <Col xs={1}>
                       <Glyphicon glyph={option.glyph}/>
@@ -88,7 +166,7 @@ export class MainMenu extends Component {
           <ButtonGroup vertical block bsSize="large" className="side-menu-button-group">
             {
               backgroundOptions.map( option =>
-                <Button onClick={this.changeBackground}>
+                <Button onClick={()=> {this.changeBackground(option.class)}}>
                   <Row>
                     <Col xs={1}>
                       <Glyphicon glyph={option.glyph}/>
@@ -103,12 +181,12 @@ export class MainMenu extends Component {
           </ButtonGroup>
         </Menu>
         {/* content (views) */}
-        { this.state.view === 'ViewArt' && <ViewArt/> }
-        { this.state.view === 'ViewArtInfo' && <ViewArtInfo/> }
-        { this.state.view === 'ViewLyrics' && <ViewLyrics/> }
+        { this.state.view === 'ViewArt' && <ViewArt background={this.state.background} albumArt={this.state.albumArt}/> }
+        { this.state.view === 'ViewArtInfo' && <ViewArtInfo background={this.state.background} albumArt={this.state.albumArt}/> }
+        { this.state.view === 'ViewLyrics' && <ViewLyrics background={this.state.background} albumArt={this.state.albumArt}/> }
         {/* about button */}
-        <Navbar fixedBottom style={style.navbar} className="nav-about">
-          { this.state.showAbout && 
+        <Navbar fixedBottom style={{...style.navbar, textAlign: 'left'}} className="nav-about">
+          { this.state.showAbout && !this.state.menuIsOpen &&
             <Button bsStyle="info" onClick={()=> this.toggleModal(true)}>About</Button>
           }
         </Navbar>
