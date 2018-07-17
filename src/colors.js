bbd196c7283af849a8b29bb30d04ea61cd4f43ec
jorgefloresq <jorge.noel.flores@gmail.com>
2018-07-17 01:06:18 -0700

    Commented code for color.js

--- src/colors.js
+++ src/colors.js
@@ -1,6 +1,7 @@
 import * as Vibrant from '@akigami/vibrant';
 
 export async function getColors(albumArt) {
+    //initialize variables which store arrays with HSL Values
     let vibrantHSL;
     let lightVibrantHSL;
     let darkVibrantHSL;
@@ -8,21 +9,30 @@ export async function getColors(albumArt) {
     let lightMutedHSL;
     let darkMutedHSL;
 
-    let colorHSLMap = [6];
+    //Array stores HSL Values
+    let colorHSLArray = [6];
 
+    //Number stored is highest population of all evaluated swatches
     let maxPop = 0;
+
+    //Store theme colors in HSL Array
     let mainColor;
     let secondaryColor;
     let gradientColor;
 
     let v = new Vibrant(albumArt);
+
+    //Promise returns a palette
     await v.getPalette().then(palette => {
 
+        //Iterate through palette and compare each type of swatch
         for (let swatch in palette) {
-            //console.log(palette[swatch]);
+
             if (swatch === 'Vibrant') {
                 vibrantHSL = palette[swatch]._hsl;
-                colorHSLMap[0] = vibrantHSL;
+                colorHSLArray[0] = vibrantHSL;
+
+                //Store population if larger than previous
                 if (palette[swatch]._population > maxPop) {
                     maxPop = palette[swatch]._population;
                     mainColor = vibrantHSL;
@@ -30,7 +40,7 @@ export async function getColors(albumArt) {
             }
             else if (swatch === 'LightVibrant') {
                 lightVibrantHSL = palette[swatch]._hsl;
-                colorHSLMap[1] = lightVibrantHSL;
+                colorHSLArray[1] = lightVibrantHSL;
                 if (palette[swatch]._population > maxPop) {
                     maxPop = palette[swatch]._population;
                     mainColor = lightVibrantHSL;
@@ -39,7 +49,7 @@ export async function getColors(albumArt) {
             }
             else if (swatch === 'DarkVibrant') {
                 darkVibrantHSL = palette[swatch]._hsl;
-                colorHSLMap[2] = darkVibrantHSL;
+                colorHSLArray[2] = darkVibrantHSL;
                 if (palette[swatch]._population > maxPop) {
                     maxPop = palette[swatch]._population;
                     mainColor = darkVibrantHSL;
@@ -48,7 +58,7 @@ export async function getColors(albumArt) {
             }
             else if (swatch === 'Muted') {
                 mutedHSL = palette[swatch]._hsl;
-                colorHSLMap[3] = darkVibrantHSL;
+                colorHSLArray[3] = darkVibrantHSL;
                 if (palette[swatch]._population > maxPop) {
                     maxPop = palette[swatch]._population;
                     mainColor = mutedHSL;
@@ -57,7 +67,7 @@ export async function getColors(albumArt) {
             }
             else if (swatch === 'LightMuted') {
                 lightMutedHSL = palette[swatch]._hsl;
-                colorHSLMap[4] = lightMutedHSL;
+                colorHSLArray[4] = lightMutedHSL;
                 if (palette[swatch]._population > maxPop) {
                     maxPop = palette[swatch]._population;
                     mainColor = lightMutedHSL;
@@ -66,19 +76,18 @@ export async function getColors(albumArt) {
             }
             else if (swatch === 'DarkMuted') {
                 darkMutedHSL = palette[swatch]._hsl;
-                colorHSLMap[5] = darkMutedHSL;
+                colorHSLArray[5] = darkMutedHSL;
                 if (palette[swatch]._population > maxPop) {
                     maxPop = palette[swatch]._population;
                     mainColor = darkMutedHSL;
                 }
-
             }
         }
-        console.log(mainColor);
-        secondaryColor = getSecondaryColor(colorHSLMap, mainColor[2]);
-        console.log(secondaryColor);
-        gradientColor = getGradientColor(colorHSLMap, mainColor[0]);
-        console.log(gradientColor);
+        //call function to compute different colors
+        secondaryColor = getSecondaryColor(colorHSLArray, mainColor[2]);
+        gradientColor = getGradientColor(colorHSLArray, mainColor[0]);
+
+        //convert all colors to hex from HSL
         let colorArray = [hslToHex(
             mainColor[0],
             mainColor[1],
@@ -95,12 +104,13 @@ export async function getColors(albumArt) {
         )];
 
 
-        console.log(colorArray);
+        //Return array of hex values
         return colorArray;
     });
 
 }
 
+//Compares main color with all colors of array to evaluate highest luminous contrast
 function getSecondaryColor(colorMap, mainLum) {
     let maxLumDiff = 0;
     let secondarySwatch;
@@ -113,6 +123,7 @@ function getSecondaryColor(colorMap, mainLum) {
     return secondarySwatch;
 };
 
+//Compares main color with all colors of array to evaluate minimum difference in hue
 function getGradientColor(colorMap, mainHue) {
     let minHueDiff = 1;
     let gradientSwatch;
@@ -125,9 +136,10 @@ function getGradientColor(colorMap, mainHue) {
     return gradientSwatch;
 }
 
+//Vibrant lacks an hsl to hex conversion method, so hsl is converted hsl, then to hex.
 function hslToHex(h ,s ,l){
     let rgb = Vibrant.Util.hslToRgb(h, s, l);
-    let hex = Vibrant.Util.rgbToHex(rgb[0],rgb[1], rgb[2]);
+    let hex = Vibrant.Util.rgbToHex(rgb[0], rgb[1], rgb[2]);
 
     return hex;
 }
