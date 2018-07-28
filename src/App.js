b06d5d6ce16e70054d18a3288a6055adb1f0d3cd
jorgefloresq <jorge.noel.flores@gmail.com>
2018-07-28 12:26:22 -0700

    Code refactored and fixed minor bugs

--- src/App.js
+++ src/App.js
@@ -2,52 +2,125 @@ import React, { Component } from 'react';
 import './App.css';
 import { MainMenu } from './components/main-menu';
 import queryString from 'query-string';
+import { defaultB, dynamic, blurred } from './components/background-styles';
+import { getColors } from './colors';
 
 class App extends Component {
   constructor(props) {
     super(props)
-
     this.state = {
       albumArt: '',
       artists: '',
-      songName: ''
-    }
+      songName: '',
+      background: 'Default',
+      backgroundStyle: { defaultB }
+    };
+    this.changeBackground = this.changeBackground.bind(this);
+    this.fetchData = this.fetchData.bind(this);
+    this.loadData = this.loadData.bind(this);
   }
 
   componentDidMount() {
+    // function fetches data, changes background and sets states
+    this.loadData();
+    // load data every 3 seconds
     setInterval(() => {
-      //Retrieve URL String
-      let parsed = queryString.parse(window.location.search);
-
-      //Stores accesstoken from URL String
-      let accessToken = parsed.access_token;
-
-      //Checks if access token is retrieved 
-      if (!accessToken)
-        return;
-      //fetch() receives endpoint and headers as arguments
-      fetch('https://api.spotify.com/v1/me/player/currently-playing', {
-        headers: { 'Authorization': 'Bearer ' + accessToken }
-      }).then(response => response.json())
-        .then(data => {
-          console.log(data)
-          //Grabs any artist in the artists array and creates string
-          let artists = '';
-          data.item.artists.forEach(artist => {
-            artists += `${artist.name}, `;
-          });
-          artists = artists.substring(0, artists.length - 2);
-
-          //console.log(data.item.album.images[0].url);
-          //console.log(artists);
-          //console.log(data.item.name);
-          this.setState({
-            albumArt: data.item.album.images[0].url,
-            artists: artists,
-            songName: data.item.name
-          });
-        });
+      this.loadData();
     }, 3000);
+  }
+
+  //Changes background colors and evaluates colors if dynamic
+  async changeBackground(background, albumArt) {
+    // background
+    let bg;
+    // font color
+    let fc;
+    // default background selected
+    if (background === 'Default') {
+      // set background
+      bg = { ...defaultB };
+      // set font color
+      fc = 'white';
+    }
+    // dynamic background selected
+    else if (background === 'Dynamic') {
+      await getColors(albumArt).then(colors => {
+        //assign colors from array
+        let mainColor = colors[0];
+        let secondaryColor = colors[1];
+        // set background
+        bg = {
+          ...dynamic,
+          background: mainColor
+        };
+        // set font color (view art info)
+        fc = secondaryColor;
+      });
+    }
+    // blurred background selected
+    else {
+      fc = 'white';
+      // set background
+      bg = {
+        background: `url(${albumArt})`,
+        color: fc,
+        ...blurred
+      };
+    }
+    // Set Background states
+    this.setState({
+      background: background,
+      backgroundStyle: bg,
+      fontColor: fc
+    })
+  }
+  async loadData() {
+    //Initial data fetch
+    let data;
+    await this.fetchData().then(result => {
+      data = result;
+    });
+    this.changeBackground(this.state.background, data.albumArt);
+    this.setState({
+      albumArt: data.albumArt,
+      artists: data.artists,
+      songName: data.songName
+    })
+  }
+
+  fetchData() {
+    //Retrieve URL String
+    let parsed = queryString.parse(window.location.search);
+
+    //Stores accesstoken from URL String
+    let accessToken = parsed.access_token;
+
+    //Checks if access token is retrieved 
+    if (!accessToken)
+      return;
+    //fetch() receives endpoint and headers as arguments
+    return fetch('https://api.spotify.com/v1/me/player/currently-playing', {
+      headers: { 'Authorization': 'Bearer ' + accessToken }
+    }).then(response => response.json())
+      .then(async data => {
+        console.log(data);
+        // get current song's id
+        let id = data.item.id;
+        console.log('song changed');
+        //Grabs any artist in the artists array and creates string
+        let artists = '';
+        data.item.artists.forEach(artist => {
+          artists += `${artist.name}, `;
+        });
+        artists = artists.substring(0, artists.length - 2);
+        let albumArt = data.item.album.images[0].url;
+        return {
+          albumArt: albumArt,
+          artists: artists,
+          songName: data.item.name,
+          id: id
+        };
+      });
   }
   render() {
     return (
@@ -56,6 +129,10 @@ class App extends Component {
           albumArt={this.state.albumArt}
           artists={this.state.artists}
           songName={this.state.songName}
+          backgroundStyle={this.state.backgroundStyle}
+          fontColor={this.state.fontColor}
+          onChangeBackground={this.changeBackground}
+          background={this.state.background}
         />
       </div>
     );
--- src/components/main-menu.js
+++ src/components/main-menu.js
@@ -42,7 +42,6 @@ export class MainMenu extends Component {
     this.toggleModal = this.toggleModal.bind(this);
     this.handleMenuButton = this.handleMenuButton.bind(this);
     this.handleMenuItem = this.handleMenuItem.bind(this);
-    this.changeBackground = this.changeBackground.bind(this);
     this.handleMenuStateChange = this.handleMenuStateChange.bind(this);
     this.handleMouseMove = this.handleMouseMove.bind(this);
     this.hideButtons = this.hideButtons.bind(this);
@@ -53,29 +52,20 @@ export class MainMenu extends Component {
       showMenu: true,
       menuIsOpen: false,
       view: 'ViewArt',
-      background: 'Default',
-      backgroundStyle: defaultB,
-      fontColor: 'white',
-      cursor: 'default',
-      //albumArt: this.props.albumArt,
-      //artists: this.props.artists,
-      //songName: this.props.songName
+      background: '',
+      cursor: 'default'
     };
   }
   // wait counter (wait 3s to hide menu buttons)
   timeout;
 
   componentDidMount() {
-      // mouse move event listener
+    // mouse move event listener
     document.addEventListener("mousemove", this.handleMouseMove);
     // hide menu buttons
     this.hideButtons();
     console.log("component did mount");
-  
-  }
-
-  componentWillReceiveProps(){
-    //this.changeBackground(this.state.background);
+    console.log(this.props.backgroundStyle);
   }
 
   handleMouseMove() {
@@ -86,16 +76,16 @@ export class MainMenu extends Component {
       showAbout: true,
       showMenu: true,
       cursor: 'default'
-    }, ()=> {
+    }, () => {
       // set timeout to hide menu buttons
       this.timeout = this.hideButtons();
-    });    
+    });
   }
 
   hideButtons() {
-    return setTimeout(()=> {
+    return setTimeout(() => {
       // hide menu buttons
-      this.setState({ 
+      this.setState({
         showAbout: false,
         showMenu: false,
         cursor: 'none'
@@ -110,17 +100,17 @@ export class MainMenu extends Component {
 
   handleMenuStateChange(state) {
     clearTimeout(this.timeout);
-    this.setState({ 
+    this.setState({
       // toggle about button
       showAbout: !state.isOpen,
       // toggle menu button
       showMenu: !state.isOpen,
       // change menu isOpen prop
       menuIsOpen: state.isOpen
-     }, ()=> {
-       // set timeout to hide menu buttons
+    }, () => {
+      // set timeout to hide menu buttons
       this.timeout = this.hideButtons();
-     });
+    });
   }
 
   handleMenuItem(view) {
@@ -129,56 +119,7 @@ export class MainMenu extends Component {
       view: view,
       // close menu
       menuIsOpen: false,
-     });    
-  }
-
-  async changeBackground(background) {
-    // background
-    let bg;
-    // font color
-    let fc;
-    // default background selected
-    if (background === 'Default') {
-      // set background
-      bg = {...defaultB};
-      // set font color (view art info)
-      fc = 'white';
-    }
-    // dynamic background selected
-    else if (background === 'Dynamic') {
-
-      await getColors(this.props.albumArt).then( colors => {
-        //assign colors from array
-        let mainColor = colors[0];
-        let secondaryColor = colors[1];
-        let gradientColor = colors[2];
-        // set background
-        bg = {
-          ...dynamic,
-          background: mainColor
-        };
-        // set font color (view art info)
-        fc = secondaryColor;
-      });
-    }
-    // blurred background selected
-    else {
-      // set background
-      bg = { 
-        background: `url(${this.props.albumArt})`,
-        ...blurred
-      };      
-    }
-    // change background
-    this.setState({ 
-      background: background,
-      // set background
-      backgroundStyle: bg,
-      // set font color
-      fontColor: fc,
-      // close menu
-      menuIsOpen: false,
-    });    
+    });
   }
 
   toggleModal(toggleValue) {
@@ -186,13 +127,13 @@ export class MainMenu extends Component {
     this.setState({ showModal: toggleValue });
   }
 
-  render() { 
+  render() {
     return (
-      <div style={{cursor: this.state.cursor}}>
+      <div style={{ cursor: this.state.cursor }}>
         {/* menu button */}
-        { this.state.showMenu && 
+        {this.state.showMenu &&
           <Button bsSize="large" onClick={this.handleMenuButton} className="nav-menu">
-            <Icon name="settings"/>
+            <Icon name="settings" />
           </Button>
         }
         {/* menu */}
@@ -201,14 +142,14 @@ export class MainMenu extends Component {
             {/* view options */}
             <ButtonGroup vertical block bsSize="large" className="side-menu-button-group">
               {
-                viewOptions.map( option =>
-                  <Button onClick={()=> this.handleMenuItem(option.view)}>
+                viewOptions.map(option =>
+                  <Button onClick={() => this.handleMenuItem(option.view)}>
                     <Row>
                       <Col xs={12}>
-                        { this.state.view === option.view ?
-                          <Icon name={option.icon} size={45} color='cyan'/> :
-                          <Icon name={option.icon}/>
-                        }                      
+                        {this.state.view === option.view ?
+                          <Icon name={option.icon} size={45} color='cyan' /> :
+                          <Icon name={option.icon} />
+                        }
                       </Col>
                     </Row>
                   </Button>
@@ -220,13 +161,19 @@ export class MainMenu extends Component {
             {/* background options */}
             <ButtonGroup vertical block bsSize="large" className="side-menu-button-group">
               {
-                backgroundOptions.map( option =>
-                  <Button onClick={()=> {this.changeBackground(option.background)}}>
+                backgroundOptions.map(option =>
+                  <Button onClick={() => { 
+                    this.props.onChangeBackground(option.background, this.props.albumArt); 
+                    this.setState({
+                      menuIsOpen: false
+
+                    }) 
+                  }}>
                     <Row>
                       <Col xs={12}>
-                        { this.state.background === option.background ?
-                          <Icon name={option.icon} size={45} color='cyan'/> :
-                          <Icon name={option.icon}/>
+                        {this.props.background === option.background ?
+                          <Icon name={option.icon} size={45} color='cyan' /> :
+                          <Icon name={option.icon} />
                         }
                       </Col>
                     </Row>
@@ -234,53 +181,53 @@ export class MainMenu extends Component {
                 )
               }
             </ButtonGroup>
-            <div style={{height: 50}}></div>
+            <div style={{ height: 50 }}></div>
           </div>
         </Menu>
         {/* content (views) */}
-        { this.state.view === 'ViewArt' && 
-          <ViewArt 
-            fontColor={this.state.fontColor} 
+        {this.state.view === 'ViewArt' &&
+          <ViewArt
+            fontColor={this.props.fontColor}
             albumArt={this.props.albumArt}
           />
         }
-        { this.state.view === 'ViewArtInfo' && 
-          <ViewArtInfo 
-            fontColor={this.state.fontColor} 
+        {this.state.view === 'ViewArtInfo' &&
+          <ViewArtInfo
+            fontColor={this.props.fontColor}
             albumArt={this.props.albumArt}
             artists={this.props.artists}
             songName={this.props.songName}
-          /> 
+          />
         }
-        { this.state.view === 'ViewLyrics' && 
-          <ViewLyrics 
-            fontColor={this.state.fontColor} 
+        {this.state.view === 'ViewLyrics' &&
+          <ViewLyrics
+            fontColor={this.props.fontColor}
             albumArt={this.props.albumArt}
             artists={this.props.artists}
             songName={this.props.songName}
-          /> 
+          />
         }
         {/* about button */}
-        { this.state.showAbout && !this.state.menuIsOpen &&
-          <Button onClick={()=> this.toggleModal(true)} className="nav-about">
-            <Icon name="about"/>
+        {this.state.showAbout && !this.state.menuIsOpen &&
+          <Button onClick={() => this.toggleModal(true)} className="nav-about">
+            <Icon name="about" />
           </Button>
         }
         {/* about modal */}
-        <Modal show={this.state.showModal} onHide={()=> this.toggleModal(false)} className="modal-about">
+        <Modal show={this.state.showModal} onHide={() => this.toggleModal(false)} className="modal-about">
           <Modal.Header>
             <Modal.Title>About</Modal.Title>
           </Modal.Header>
-          <Modal.Body>          
+          <Modal.Body>
             <p>
               Spotify ArtWork Website for displaying music info, albumart and lyrics.
             </p>
           </Modal.Body>
           <Modal.Footer>
-            <Button onClick={()=> this.toggleModal(false)}>Close</Button>
+            <Button onClick={() => this.toggleModal(false)}>Close</Button>
           </Modal.Footer>
         </Modal>
-        <div id="backgroundDiv" style={this.state.backgroundStyle}></div>
+        <div id="backgroundDiv" style={this.props.backgroundStyle}></div>
       </div>
     );
   }
