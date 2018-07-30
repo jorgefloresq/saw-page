0ad8b5dd3716388614ee1c7aa62cdcbe287a602a
Gio <giovanni.sanchez@cetys.edu.mx>
2018-07-30 14:31:09 -0700

    added font shadow when blurred is selected

--- src/components/view-art-info.js
+++ src/components/view-art-info.js
@@ -2,8 +2,12 @@ import React, { Component } from 'react';
 import { SongInfo } from './song-info';
 import '../css/view-art-info.css';
 
-export class ViewArtInfo extends Component {  
+export class ViewArtInfo extends Component {
   render() {
+    let textShadow;
+    if (this.props.backgroundType === 'Blurred') {
+      textShadow = '2px 2px 5px rgb(70, 70, 70)';
+    }
     return (
       <div>
         <div className="vai-container">
@@ -11,7 +15,7 @@ export class ViewArtInfo extends Component {
             <div className="vai-art">
               <img src={this.props.albumArt}/>
             </div>
-            <div id="textDiv" style={{color: this.props.fontColor}} className="vai-info">
+            <div id="textDiv" style={{color: this.props.fontColor, textShadow: textShadow}} className="vai-info">
               <SongInfo 
                 songName={this.props.songName}
                 artists={this.props.artists}
